7dd759ed282c11d1e2c3a26926c2883d944087ba
Max W <mwinter@wesleyan.edu>
2021-10-20 18:04:29 -0500

    added highscore to local storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,10 +24,16 @@ var fightOrSkip = function() {
 };
 
 var fight = function(enemy) {
+    var isPlayerTurn = true;
+    if (Math.random() > .5) {
+        isPlayerTurn = false;
+    }
 
     while(enemy.health > 0 && playerInfo.health > 0) {
-        if (fightOrSkip()) {
-            break;
+        if (isPlayerTurn) {
+            if (fightOrSkip()) {
+                break;
+            }
         }
         //var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
 
@@ -76,6 +82,8 @@ var fight = function(enemy) {
             else {
                 window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
             }
+
+            isPlayerTurn = !isPlayerTurn;
         }
     };
 
@@ -112,12 +120,53 @@ var startGame = function() {
 
 //function to end the entire game
 var endGame = function() {
-    //if player is still alive, player wins!
+
+    window.alert("The game has now ended. Let's see how you did!");
+
+     // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    // if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    } 
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+
+    // ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        startGame();
+    } 
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+
+    /* //if player is still alive, player wins!
     if (playerInfo.health > 0){
         window.alert("Great job, you've survived the game! you now have a score of " + playerInfo.money +".");
     }
     else {
-        window.alert("You've lost your robot in battel.");
+        window.alert("You've lost your robot in battle.");
+    }
+
+    //local storage check
+    var highScore = localStorage.getItem(playerInfo.name);
+    if (playerInfo.money > highScore) {
+        window.alert("You got a new highscore of " + playerInfo.money + "!");
+        localStorage.setItem(playerInfo.name, playerInfo.money);
+    }
+    else{
+        window.alert("Didn't beat your high score this time.");
     }
 
     //ask player if they want to play again
@@ -130,7 +179,7 @@ var endGame = function() {
     else {
         window.alert("Thank you for playing. Come back soon!");
     }
-};
+}; */
 
 
 var shop = function() {
