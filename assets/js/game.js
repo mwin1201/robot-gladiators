ce14e95e0e3854dc79649929d3f2ee37b45bc648
Max W <mwinter@wesleyan.edu>
2021-10-20 16:18:24 -0500

    added fight or skip function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,37 @@
+var fightOrSkip = function() {
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
+
+    promptFight = promptFight.toLowerCase();
+
+    if (promptFight === "" || promptFight === null){
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+
+    if (promptFight === "skip" || promptFight === "SKIP") {
+        //confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        if (confirmSkip){
+            window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+            playerInfo.money = Math.max(0, playerInfo.money - 10);
+            
+            return true;
+        }
+    }
+    return false;   
+
+};
+
 var fight = function(enemy) {
 
     while(enemy.health > 0 && playerInfo.health > 0) {
-        
-        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
+        if (fightOrSkip()) {
+            break;
+        }
+        //var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
 
-        if (promptFight === "skip" || promptFight === "SKIP") {
+        /* if (promptFight === "skip" || promptFight === "SKIP") {
             //confirm player wants to skip
             var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
@@ -13,7 +40,7 @@ var fight = function(enemy) {
                 playerInfo.money = Math.max(0, playerInfo.money - 10);
                 break;
             }
-        }   
+        }    */
             // generate random damage value based on player's attack power
             var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
